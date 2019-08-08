18e7f1b058f640e68b41c34588f99d58740649c0
adrian radice <adrianradice@MacBook-Air-de-adrian.local>
2019-08-08 15:02:43 -0300

    update format input

--- scripts/get_formato_entrada_fasstext.py
+++ scripts/get_formato_entrada_fasstext.py
@@ -8,26 +8,12 @@ def main():
     ap = argparse.ArgumentParser()
     ap.add_argument('sentences')
     ap.add_argument('labels', nargs='?')
-
     args = ap.parse_args()
-
     sentence_data = open(args.sentences, 'r')
-    if args.labels:
-        label_data = open(args.labels, 'r')
-        for sentence, label in zip(it_sentences(sentence_data), it_labels(label_data)):
-            # Tenemos la oración en sentence con su categoría en label
-            print("__label__"+label,",", sentence)
-            pass
-    else:
-
-        model = fasttext.load_model("model_filename.bin")
-        for sentence in it_sentences(sentence_data):
-            # Tenemos una oración en sentence
-
-            print(model.predict(sentence)[0][0]) ##.replace("__label__","",1)
-
-            pass
-    
+    label_data = open(args.labels, 'r')
+    for sentence, label in zip(it_sentences(sentence_data), it_labels(label_data)):
+        print("__label__"+label,",", sentence)
+        pass
 
 def it_sentences(sentence_data):
     for line in sentence_data:
