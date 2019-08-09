d5f50c21dad17ef17269564160f99ecf601096d2
adrianRadice <radiceadriann@gmail.com>
2019-08-08 22:57:00 -0300

    add comillas

--- scripts/probarModelo.sh
+++ scripts/probarModelo.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-python probar_modelo.py entradas/devInputFt.txt $1
-python probar_modelo.py entradas/trainInputFt.txt $1
+python probar_modelo.py entradas/devInputFt.txt "$1"
+python probar_modelo.py entradas/trainInputFt.txt "$1"
